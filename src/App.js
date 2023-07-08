be4c895d1dd7f37e120c64de834382f6996a3759
ashikam <ashikam@pg.cusat.ac.in>
2023-07-09 04:22:27 +0530

    editting frontpage

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Navbar from './Navbar';
 import Home from './Home';
 import Modal from './Modal';
 import ViewMarks from './ViewMarks';
@@ -8,7 +7,6 @@ import ViewMarks from './ViewMarks';
 function App() {
   return (
     <div>
-      <Navbar />
       <Home />
       <Modal />
       <ViewMarks />
