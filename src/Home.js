321de11425a862671e6d0eecbb5ec2721c7b0315
ashikam <ashikam@pg.cusat.ac.in>
2023-07-09 02:43:43 +0530

    reverted back to error

--- src/Home.js
+++ src/Home.js
@@ -1,50 +1,34 @@
-import React, { useState } from "react";
-import ReactDOM from "react-dom";
+import React, { useState } from 'react';
 
 function Home() {
   const [students, setStudents] = useState([]);
 
-  const handleAdd = () => {
-    const studentName = document.getElementById("studentName").value;
-    if (studentName === "") {
-      return;
-    }
-
-    setStudents([...students, {
-      index: students.length + 1,
-      name: studentName,
-    }]);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const studentName = e.target.elements.student.value;
+    setStudents([...students, { id: students.length + 1, name: studentName }]);
+    e.target.elements.student.value = '';
   };
 
   return (
     <div>
-      <h1>Students List</h1>
-      <table>
-        <thead>
-          <tr>
-            <th>Index</th>
-            <th>Name</th>
-            <th>Action</th>
-          </tr>
-        </thead>
-        <tbody>
-          {students.map((student, index) => (
-            <tr key={index}>
-              <td>{student.index}</td>
-              <td>{student.name}</td>
-              <td>
-                <button>View Marks</button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-      <input
-        type="text"
-        id="studentName"
-        placeholder="Enter student name"
-      />
-      <button onClick={handleAdd}>Add</button>
+      <h2>Students</h2>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="student">Enter student name</label>
+        <input type="text" id="student" />
+        <button type="submit">Add</button>
+      </form>
+      <ul>
+        {students.map((student) => {
+          return (
+            <li key={student.id}>
+              {student.name} <button>View Marks</button>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
+
+export default Home;
