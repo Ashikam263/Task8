a4f5c0dc5d72b5aed84306f72fe8065d58ac2eeb
ashikam <ashikam@pg.cusat.ac.in>
2023-07-09 02:32:38 +0530

    bd

--- src/Home.js
+++ src/Home.js
@@ -1,34 +1,50 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
 
 function Home() {
   const [students, setStudents] = useState([]);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const studentName = e.target.elements.student.value;
-    setStudents([...students, { id: students.length + 1, name: studentName }]);
-    e.target.elements.student.value = '';
+  const handleAdd = () => {
+    const studentName = document.getElementById("studentName").value;
+    if (studentName === "") {
+      return;
+    }
+
+    setStudents([...students, {
+      index: students.length + 1,
+      name: studentName,
+    }]);
   };
 
   return (
     <div>
-      <h2>Students</h2>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="student">Enter student name</label>
-        <input type="text" id="student" />
-        <button type="submit">Add</button>
-      </form>
-      <ul>
-        {students.map((student) => {
-          return (
-            <li key={student.id}>
-              {student.name} <button>View Marks</button>
-            </li>
-          );
-        })}
-      </ul>
+      <h1>Students List</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Index</th>
+            <th>Name</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {students.map((student, index) => (
+            <tr key={index}>
+              <td>{student.index}</td>
+              <td>{student.name}</td>
+              <td>
+                <button>View Marks</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <input
+        type="text"
+        id="studentName"
+        placeholder="Enter student name"
+      />
+      <button onClick={handleAdd}>Add</button>
     </div>
   );
 }
-
-export default Home;
