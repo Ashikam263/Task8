5a06c25ad764cb05fe73a4500a382a5520875f08
ashikam <ashikam@pg.cusat.ac.in>
2023-07-09 14:58:00 +0530

    react-router -dom

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Home from './Home';
-import Modal from './Modal';
+import Title from './Title';
+import Table from './Table';
 import ViewMarks from './ViewMarks';
+import Modal from './Modal';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
+  
   return (
-    <div>
-      <Home />
-      <Modal />
-      <ViewMarks />
-    </div>
+    <Router>
+      <div className="App">
+        <Title />
+        <Switch>
+          <Route exact path="/">
+            <Table />
+          </Route>
+          <Route path="/ViewMarks/:id">
+            <ViewMarks />
+          </Route> 
+          <Route path="/Modal">
+            <Modal />
+          </Route> 
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+export default App;
